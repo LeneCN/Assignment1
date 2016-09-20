954028550c54886e2ad47a91e7b9ae78b9a46944
LeneCN <lenececilienielsen@hotmail.com>
2016-09-20 23:02:47 +0200

    Create MapActivity2.java

---
+++ MapActivity2.java
@@ -0,0 +1,17 @@
+package com.assignment.lenececilienielsen.mapapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MapActivity2 extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_map);
+    }
+
+}
