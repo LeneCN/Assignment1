137de63c935c7cd39f8d2cdfc481de667b1a7ebf
LeneCN <lenececilienielsen@hotmail.com>
2016-09-21 12:12:27 +0200

    Update MapActivity2.java

--- MapActivity2.java
+++ MapActivity2.java
@@ -11,7 +11,9 @@ public class MapActivity2 extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity2_map);
+        setContentView(R.layout.activity2_map); //uses the layout from the xml code activty2
     }
 
 }
+
+//https://www.mkyong.com/android/android-activity-from-one-screen-to-another-screen/ I have used some code from this website on the code above
